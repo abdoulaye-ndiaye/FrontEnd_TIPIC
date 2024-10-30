eeaf32a3bc1dc6662184520a29b26bc3b7fa5be5
Sidy TRAORE <generaltra10@gmail.com>
2024-10-30 22:16:27 +0100

    ajout du guard pour la protection des routes en cas de non connection

--- statert-kit/src/app/app.config.ts
+++ statert-kit/src/app/app.config.ts
@@ -1,29 +1,34 @@
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+    HttpClient,
+    provideHttpClient,
+    withInterceptors,
+} from "@angular/common/http";
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { authInterceptor } from "./interceptors/auth.interceptor";
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+    return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimations(),
-    provideHttpClient(),
-    provideRouter(routes),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      })
-    )
-  ]
+    providers: [
+        provideAnimations(),
+        provideHttpClient(withInterceptors([authInterceptor])),
+        provideRouter(routes),
+        importProvidersFrom(
+            TranslateModule.forRoot({
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: HttpLoaderFactory,
+                    deps: [HttpClient],
+                },
+            })
+        ),
+    ],
 };
--- statert-kit/src/app/app.routes.ts
+++ statert-kit/src/app/app.routes.ts
@@ -1,21 +1,23 @@
-import { Routes } from '@angular/router';
-import { ContentComponent } from './shared/component/layout/content/content.component';
-import { dashData } from './shared/routes/routes';
-import { LoginComponent } from './auth/login/login.component';
+import { Routes } from "@angular/router";
+import { ContentComponent } from "./shared/component/layout/content/content.component";
+import { dashData } from "./shared/routes/routes";
+import { LoginComponent } from "./auth/login/login.component";
+import { authGuard } from "./guard/auth.guard";
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/pages/sample-page1',
-    pathMatch: 'full'
-  },
-  {
-    path: 'auth/login',
-    component: LoginComponent,
-  },
-  {
-    path: '',
-    component: ContentComponent,
-    children: dashData
-  },
+    {
+        path: "",
+        redirectTo: "auth/login",
+        pathMatch: "full",
+    },
+    {
+        path: "auth/login",
+        component: LoginComponent,
+    },
+    {
+        path: "",
+        component: ContentComponent,
+        children: dashData,
+        canActivate: [authGuard],
+    },
 ];
