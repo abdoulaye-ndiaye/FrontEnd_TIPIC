d8c7d3b0e10ebe0994538158ab759bc514ad03c7
Sidy TRAORE <generaltra10@gmail.com>
2024-10-31 20:26:26 +0100

    Refactoriser le code HTML de la page utilisateur.component.html

--- statert-kit/src/app/services/utilisateur.service.ts
+++ statert-kit/src/app/services/utilisateur.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { jwtDecode } from "jwt-decode";
 import { environment } from "../environments/environment";
 
 @Injectable({
@@ -8,38 +7,40 @@ import { environment } from "../environments/environment";
 })
 export class UtilisateurService {
     constructor(private httpClient: HttpClient) {}
-
-    getDecodedToken(token: string) {
-        try {
-            return jwtDecode(token);
-        } catch (Error) {
-            return null;
-        }
-    }
-
     getAll() {
         return this.httpClient.get<any>(`${environment.apiUrl}/utilisateur`);
     }
 
     getById(id: string) {
-        return this.httpClient.get<any>(`${environment.apiUrl}/utilisateur/${id}`);
+        return this.httpClient.get<any>(
+            `${environment.apiUrl}/utilisateur/${id}`
+        );
     }
-   // bloquer un utilisateur
+    // bloquer un utilisateur
     bloquer(id: string) {
-        return this.httpClient.put<any>(`${environment.apiUrl}/bloquer-utilisateur/${id}`, {});
+        return this.httpClient.put<any>(
+            `${environment.apiUrl}/bloquer-utilisateur/${id}`,
+            {}
+        );
     }
     // debloquer un utilisateur
     debloquer(id: string) {
-        return this.httpClient.put<any>(`${environment.apiUrl}/debloquer-utilisateur/${id}`, {});
+        return this.httpClient.put<any>(
+            `${environment.apiUrl}/debloquer-utilisateur/${id}`,
+            {}
+        );
     }
     // supprimer un utilisateur
     delete(id: string) {
-        return this.httpClient.delete<any>(`${environment.apiUrl}/utilisateur/${id}`);
+        return this.httpClient.delete<any>(
+            `${environment.apiUrl}/utilisateur/${id}`
+        );
     }
     // modifier un utilisateur
     update(id: string, utilisateur: any) {
-        return this.httpClient.put<any>(`${environment.apiUrl}/utilisateur/${id}`, utilisateur);
+        return this.httpClient.put<any>(
+            `${environment.apiUrl}/utilisateur/${id}`,
+            utilisateur
+        );
     }
-    
-    
 }
