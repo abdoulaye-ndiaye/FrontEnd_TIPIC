de6be57606d87b5e0bf8ea4faca8c0b14f0a090d
Sidy TRAORE <generaltra10@gmail.com>
2024-10-31 20:25:27 +0100

    correction du logout

--- statert-kit/src/app/shared/component/header/profile/profile.component.ts
+++ statert-kit/src/app/shared/component/header/profile/profile.component.ts
@@ -1,43 +1,40 @@
-import { UtilisateurService } from './../../../../services/utilisateur.service';
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
-import { FeathericonComponent } from '../../feathericon/feathericon.component';
+import { FeathericonComponent } from "../../feathericon/feathericon.component";
+import { AuthService } from "../../../../services/auth.service";
 
 @Component({
-  selector: 'app-profile',
-  standalone: true,
-  imports: [CommonModule, FeathericonComponent, RouterModule],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+    selector: "app-profile",
+    standalone: true,
+    imports: [CommonModule, FeathericonComponent, RouterModule],
+    templateUrl: "./profile.component.html",
+    styleUrl: "./profile.component.scss",
 })
 export class ProfileComponent implements OnInit {
+    public show: boolean = false;
+    profile: any;
+    token: string;
+    nom: any;
+    prenom: any;
+    role: any;
 
-  public show: boolean = false;
-  profile: any;
-  token: string;
-  nom: any;
-  prenom: any;
-  role: any;
+    constructor(private authService: AuthService) {}
 
-  constructor(private router: Router,private UtilisateurService:UtilisateurService) { }
+    ngOnInit(): void {
+        const token = localStorage.getItem("token") as string;
+        this.profile = this.authService.getDecodedToken(token);
+        this.nom = this.profile.user.nom;
+        this.prenom = this.profile.user.prenom;
+        this.role = this.profile.user.role;
+    }
 
-  ngOnInit(): void {
-    const token = localStorage.getItem("token") as string;
-    this.profile = this.UtilisateurService.getDecodedToken(token);
-    this.nom = this.profile.user.nom;
-    this.prenom = this.profile.user.prenom;
-    this.role = this.profile.user.role;
-  }
+    open() {
+        this.show = !this.show;
+    }
 
-  open() {
-    this.show = !this.show
-  }
-
-  logout() {
-    localStorage.removeItem("token");
-    this.router.navigate(["/auth/login"]);
-  }
-
+    logout() {
+        this.authService.logout();
+    }
 }
