66036d2e7370be9d88e8121ed11d6615a8bbd2b7
Sidy TRAORE <generaltra10@gmail.com>
2024-11-06 02:49:46 +0100

    page ajout sift-Ms avec download si déjà éxistant

--- statert-kit/src/app/component/iprem/ajout-sift-ms/ajout-sift-ms.component.ts
+++ statert-kit/src/app/component/iprem/ajout-sift-ms/ajout-sift-ms.component.ts
@@ -6,53 +6,120 @@ import {
     ReactiveFormsModule,
     Validators,
 } from "@angular/forms";
-import { DropzoneModule } from "ngx-dropzone-wrapper";
-import { DropzoneConfigInterface } from "ngx-dropzone-wrapper";
 import Swal from "sweetalert2";
 import { SiftMsService } from "../../../services/sift-ms.service";
+import { ActivatedRoute } from "@angular/router";
+import { FromageService } from "../../../services/fromage.service";
 
 @Component({
     selector: "app-ajout-sift-ms",
     standalone: true,
-    imports: [DropzoneModule, ReactiveFormsModule, CommonModule],
+    imports: [ReactiveFormsModule, CommonModule],
     templateUrl: "./ajout-sift-ms.component.html",
     styleUrl: "./ajout-sift-ms.component.scss",
 })
 export class AjoutSiftMsComponent implements OnInit {
     siftMsForm: FormGroup;
     validate = false;
+    fileSelected: File;
+    nom: string;
+    id_echantillon: string;
+    fromage: any;
+    siftMs: any;
 
     constructor(
         private formBuilder: FormBuilder,
-        private siftMsService: SiftMsService
+        private siftMsService: SiftMsService,
+        private route: ActivatedRoute,
+        private fromageService: FromageService
     ) {}
 
     ngOnInit(): void {
+        this.route.queryParams.subscribe((params) => {
+            this.id_echantillon = params["id"];
+        });
+
+        this.fromageService
+            .getEchantillonById(this.id_echantillon)
+            .subscribe((data) => {
+                this.fromage = data;
+            });
+        this.siftMsService
+            .getByIdEchantilon(this.id_echantillon)
+            .subscribe((data) => {
+                console.log(data);
+
+                this.siftMs = data[0];
+            });
+
         this.siftMsForm = this.formBuilder.group({
             blanc_positif: ["", Validators.required],
             blanc_negatif: ["", Validators.required],
             fromage_positif: ["", Validators.required],
             fromage_negatif: ["", Validators.required],
-            file: [null, Validators.required],
+            file: ["", Validators.required],
         });
     }
 
-    onSubmit() {}
+    onSubmit() {
+        this.validate = true;
+        this.nom = this.fileSelected.name;
+        if (this.siftMsForm.valid) {
+            console.log(this.siftMsForm.value);
+            const formData = new FormData();
+            formData.append(
+                "blanc_positif",
+                this.siftMsForm.value.blanc_positif
+            );
+            formData.append(
+                "blanc_negatif",
+                this.siftMsForm.value.blanc_negatif
+            );
+            formData.append(
+                "fromage_positif",
+                this.siftMsForm.value.fromage_positif
+            );
+            formData.append(
+                "fromage_negatif",
+                this.siftMsForm.value.fromage_negatif
+            );
+            formData.append("fichier", this.fileSelected, this.nom);
+            formData.append("echantillonId", this.id_echantillon);
 
-    public Config: DropzoneConfigInterface = {
-        clickable: true,
-        url: "https://httpbin.org/post",
-        autoProcessQueue: true,
-        addRemoveLinks: true,
-        parallelUploads: 1,
-    };
+            this.siftMsService.upload(formData).subscribe(
+                (response) => {
+                    Swal.fire({
+                        icon: "success",
+                        title: "Succès",
+                        text: "SIFT-MS ajouté avec succès.",
+                    });
+                    this.reset();
+                    //console.log(response);
+                },
+                (error) => {
+                    //console.log(error);
 
-    public text =
-        ' <div class="dz-message needsclick"><i class="icon-cloud-up"></i><p>Déposez les fichiers ici ou cliquez pour les ajouter.</p></div>';
-
-    onUploadError(args: DropzoneConfigInterface): void {}
-
-    onUploadSuccess(args: DropzoneConfigInterface): void {}
+                    Swal.fire({
+                        icon: "error",
+                        title: "Erreur",
+                        text: "Erreur lors de l'ajout du SIFT-MS.",
+                    });
+                }
+            );
+        }
+    }
+    telecharger(id: string) {
+        this.siftMsService.dowloadSiftMs(id);
+    }
+    onFileChange(event: any) {
+        if (event.target.files[0]) {
+            this.fileSelected = event.target.files[0] as File;
+        }
+    }
+    reset() {
+        this.siftMsForm.reset();
+        this.siftMsForm.setErrors(null);
+    }
 
     get blanc_positif() {
         return this.siftMsForm.get("blanc_positif");
--- statert-kit/src/app/services/sift-ms.service.ts
+++ statert-kit/src/app/services/sift-ms.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "../environments/environment";
+import Swal from "sweetalert2";
 
 @Injectable({
     providedIn: "root",
@@ -9,9 +10,23 @@ export class SiftMsService {
     constructor(private httpClient: HttpClient) {}
 
     upload(data: any) {
-        return this.httpClient.post<any>(
-            `${environment.apiUrl}/upload-article`,
-            data
+        return this.httpClient.post<any>(`${environment.apiUrl}/siftMs`, data);
+    }
+
+    getByIdEchantilon(id: string) {
+        return this.httpClient.get<any>(
+            `${environment.apiUrl}/echantillon/${id}/siftMs`
         );
+    }
+
+    dowloadSiftMs(id: string) {
+        this.httpClient
+            .get(`${environment.apiUrl}/download-siftMs/${id}`, {
+                responseType: "blob",
+            })
+            .subscribe((res) => {
+                window.open(window.URL.createObjectURL(res));
+                Swal.close();
+            });
     }
 }
