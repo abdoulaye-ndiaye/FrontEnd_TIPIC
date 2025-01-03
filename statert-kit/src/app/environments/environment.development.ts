eecb405c178433721d3b2371d2ce47cd468dc57b
Sidy TRAORE <generaltra10@gmail.com>
2025-01-03 18:49:03 +0100

    Update API URL in environment files

--- statert-kit/src/app/environments/environment.development.ts
+++ statert-kit/src/app/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     //apiUrl: 'http://141.94.115.246:3002',
-    apiUrl: "http://tipic_api:8000",
+    apiUrl: "http://tipic.univ-pau.fr:8000",
 };
