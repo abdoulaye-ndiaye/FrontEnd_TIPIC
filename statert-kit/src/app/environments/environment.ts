3bdb3bb2a76e25945f9b3f5ed6e7721dfc58589c
Sidy TRAORE <generaltra10@gmail.com>
2025-01-15 00:27:18 +0100

    Update API URL to use HTTPS

--- statert-kit/src/app/environments/environment.ts
+++ statert-kit/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     //apiUrl: 'http://141.94.115.246:3002',
-    apiUrl: "http://tipic.univ-pau.fr:8000",
+    apiUrl: "https://tipic.univ-pau.fr/api",
 };
