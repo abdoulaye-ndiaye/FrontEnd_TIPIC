27853aade8a8941ed5bfb58e7b4ddf3806b47231
Sidy TRAORE <generaltra10@gmail.com>
2025-01-29 01:00:41 +0100

    dashboard producteur dynamique

--- statert-kit/src/app/component/producteur/dashbord-producteur/dashbord-producteur.component.ts
+++ statert-kit/src/app/component/producteur/dashbord-producteur/dashbord-producteur.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { AuthService } from "../../../services/auth.service";
 import { jwtDecode } from "jwt-decode";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
@@ -22,6 +21,8 @@ import {
     ApexAnnotations,
     ApexGrid,
 } from "ng-apexcharts";
+import { FromageService } from "../../../services/fromage.service";
+import { UtilisateurService } from "../../../services/utilisateur.service";
 
 export type ChartOptions = {
     series?: ApexAxisChartSeries;
@@ -51,191 +52,13 @@ let primary_color = localStorage.getItem("primary_color") || "#5C61F2";
 let secondary_color = localStorage.getItem("secondary_color") || "#FF9766";
 
 @Component({
-  selector: 'app-dashbord-producteur',
-  standalone: true,
-  imports: [CommonModule, RouterModule, NgApexchartsModule],
-  templateUrl: './dashbord-producteur.component.html',
-  styleUrl: './dashbord-producteur.component.scss'
+    selector: "app-dashbord-producteur",
+    standalone: true,
+    imports: [CommonModule, RouterModule, NgApexchartsModule],
+    templateUrl: "./dashbord-producteur.component.html",
+    styleUrl: "./dashbord-producteur.component.scss",
 })
 export class DashbordProducteurComponent {
- @Input() TotalUtilisateur: ChartOptions | any = {
-        series: [
-            {
-                name: "Desktops",
-                data: [10, 35, 15, 78, 40, 60, 12, 60],
-            },
-        ],
-        chart: {
-            type: "area",
-            height: 120,
-            offsetY: 50,
-            zoom: {
-                enabled: false,
-            },
-            toolbar: {
-                show: false,
-            },
-            dropShadow: {
-                enabled: true,
-                top: 5,
-                left: 0,
-                bottom: 3,
-                blur: 2,
-                color: "#61AE41",
-                opacity: 0.2,
-            },
-        },
-        colors: ["#61AE41"],
-        fill: {
-            type: "gradient",
-            gradient: {
-                shadeIntensity: 1,
-                opacityFrom: 0.6,
-                opacityTo: 0.2,
-                stops: [0, 100, 100],
-            },
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        grid: {
-            show: false,
-        },
-        xaxis: {
-            labels: {
-                show: false,
-            },
-            axisBorder: {
-                show: false,
-            },
-            axisTicks: {
-                show: false,
-            },
-        },
-        yaxis: {
-            show: false,
-        },
-        stroke: {
-            curve: "straight",
-            width: 2,
-        },
-        tooltip: {
-            custom: function ({ series, seriesIndex, dataPointIndex }: any) {
-                return (
-                    '<div class="apex-tooltip p-2">' +
-                    "<span>" +
-                    '<span class="bg-primary">' +
-                    "</span>" +
-                    "customer" +
-                    "<h3>" +
-                    "$" +
-                    series[seriesIndex][dataPointIndex] +
-                    "<h3/>" +
-                    "</span>" +
-                    "</div>"
-                );
-            },
-        },
-
-        title: "Utilisateurs",
-        class: "total-customer",
-        chartclass: "customer-chart",
-        price: "4",
-        arrowicon: "arrow-chart-up",
-        icon: "Customer",
-        percentage: "+24%",
-        color: "success",
-    };
-
-    @Input() TotalProducteur: ChartOptions | any = {
-        series: [
-            {
-                name: "Desktops",
-                data: [10, 35, 15, 78, 40, 60, 12, 60],
-            },
-        ],
-        chart: {
-            type: "area",
-            height: 120,
-            offsetY: 50,
-            zoom: {
-                enabled: false,
-            },
-            toolbar: {
-                show: false,
-            },
-            dropShadow: {
-                enabled: true,
-                top: 5,
-                left: 0,
-                bottom: 3,
-                blur: 2,
-                color: "#61AE41",
-                opacity: 0.2,
-            },
-        },
-        colors: ["#61AE41"],
-        fill: {
-            type: "gradient",
-            gradient: {
-                shadeIntensity: 1,
-                opacityFrom: 0.6,
-                opacityTo: 0.2,
-                stops: [0, 100, 100],
-            },
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        grid: {
-            show: false,
-        },
-        xaxis: {
-            labels: {
-                show: false,
-            },
-            axisBorder: {
-                show: false,
-            },
-            axisTicks: {
-                show: false,
-            },
-        },
-        yaxis: {
-            show: false,
-        },
-        stroke: {
-            curve: "straight",
-            width: 2,
-        },
-        tooltip: {
-            custom: function ({ series, seriesIndex, dataPointIndex }: any) {
-                return (
-                    '<div class="apex-tooltip p-2">' +
-                    "<span>" +
-                    '<span class="bg-primary">' +
-                    "</span>" +
-                    "customer" +
-                    "<h3>" +
-                    "$" +
-                    series[seriesIndex][dataPointIndex] +
-                    "<h3/>" +
-                    "</span>" +
-                    "</div>"
-                );
-            },
-        },
-
-        title: "Total Producteur",
-        class: "total-customer",
-        chartclass: "customer-chart",
-        price: "2",
-        arrowicon: "arrow-chart-up",
-        icon: "Customer",
-        percentage: "+12%",
-        color: "success",
-    };
-
     @Input() TotalEchantillon: ChartOptions | any = {
         series: [
             {
@@ -323,98 +146,15 @@ export class DashbordProducteurComponent {
         percentage: "+42%",
         color: "success",
     };
-    @Input() TotalSensoriel: ChartOptions | any = {
-        series: [
-            {
-                name: "Desktops",
-                data: [
-                    50, 50, 50, 25, 25, 25, 2, 2, 2, 25, 25, 25, 62, 62, 62, 35,
-                    35, 35, 66, 66,
-                ],
-            },
-        ],
-        chart: {
-            height: 100,
-            type: "area",
-            zoom: {
-                enabled: false,
-            },
-            toolbar: {
-                show: false,
-            },
-            dropShadow: {
-                enabled: true,
-                top: 5,
-                left: 0,
-                bottom: 3,
-                blur: 2,
-                color: "#44A8D7",
-                opacity: 0.2,
-            },
-        },
-        fill: {
-            type: "gradient",
-            gradient: {
-                shadeIntensity: 1,
-                opacityFrom: 0.5,
-                opacityTo: 0.1,
-                stops: [0, 90, 100],
-            },
-        },
-        tooltip: {
-            enabled: false,
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        grid: {
-            show: false,
-        },
-        xaxis: {
-            labels: {
-                show: false,
-            },
-            axisBorder: {
-                show: false,
-            },
-            axisTicks: {
-                show: false,
-            },
-        },
-        yaxis: {
-            show: false,
-        },
-        stroke: {
-            curve: "straight",
-            width: 2,
-        },
-        markers: {
-            discrete: [
-                {
-                    seriesIndex: 0,
-                    dataPointIndex: 12,
-                    fillColor: "#44A8D7",
-                    strokeColor: "#44A8D7",
-                    size: 5,
-                    shape: "circle",
-                },
-            ],
-        },
 
-        title: "Total Sensoriel",
-        class: "total-product",
-        chartclass: "total-product-chart",
-        price: "7",
-        arrowicon: "arrow-chart-up",
-        icon: "Product",
-        percentage: "+42%",
-        color: "success",
-    };
-
-    constructor(private authService: AuthService) {}
+    constructor(
+        private fromageService: FromageService,
+        private utilisateurService: UtilisateurService
+    ) {}
     nom: string;
     prenom: string;
     decodedToken: any;
+    codeProducteur: string;
 
     ngOnInit(): void {
         const token = localStorage.getItem("token") as string;
@@ -422,5 +162,40 @@ export class DashbordProducteurComponent {
         //console.log(this.decodedToken);
         this.nom = this.decodedToken.user.nom;
         this.prenom = this.decodedToken.user.prenom;
+        this.utilisateurService.getById(this.decodedToken.user._id).subscribe(
+            (response) => {
+                //console.log(response);
+                this.codeProducteur = response.codeProducteur;
+                this.fetchEchantillonsProducteur(this.codeProducteur);
+            },
+            (error) => {
+                console.error(
+                    "Erreur lors de la récupération de l'utilisateur",
+                    error
+                );
+            }
+        );
+    }
+
+    fetchEchantillonsProducteur(codeProducteur: string): void {
+        this.fromageService.getAll().subscribe(
+            (response: any[]) => {
+                // Filtrer les échantillons pour ce producteur
+                const echantillonsProducteur = response.filter(
+                    (echantillon) =>
+                        echantillon.codeProducteur === codeProducteur
+                );
+
+                // Mettre à jour le tableau de bord
+                this.TotalEchantillon.price = echantillonsProducteur.length;
+                this.TotalEchantillon.percentage = "+5%"; // Exemple de mise à jour dynamique.
+            },
+            (error) => {
+                console.error(
+                    "Erreur lors de la récupération des échantillons du producteur",
+                    error
+                );
+            }
+        );
     }
 }
--- statert-kit/src/app/environments/environment.ts
+++ statert-kit/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     //apiUrl: 'http://141.94.115.246:3002',
-    //apiUrl: "https://tipic.univ-pau.fr/api",
-    apiUrl: "http://localhost:8000",
+    apiUrl: "https://tipic.univ-pau.fr/api",
+    //apiUrl: "http://localhost:8000",
 };
